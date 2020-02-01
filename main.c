201a5badea5d576c98455dd63942adab10623f05
poly000 <34085039+poly000@users.noreply.github.com>
2020-02-01 09:30:30 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+int main(void) {
+  register int q,o;
+  {
+    int question_amount;
+    int option_amount;
+    scanf("%d%d",&option_amount,&question_amount);
+    srand(time(0));
+    q = question_amount;
+    o = option_amount;
+  }
+  for (register int i=1; i<=q; i++) {
+    printf("%d.: %c\n",i,'A'+rand()%o);
+    srand(rand());
+  }
+  getchar(),getchar();
+  return 0;
+}
